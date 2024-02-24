fa271583236022cb4cfcd71101ae244c3e738d7e
redennessy14 <136608186+redennessy14@users.noreply.github.com>
2024-02-24 16:04:28 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+import { validationResult } from "express-validator";
+import { registerValidation } from "./validations/auth.js";
+
+mongoose
+  .connect(
+    "mongodb+srv://redennessy:Nortenos67@cluster0.fvxmkix.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    console.log("db ok");
+  })
+  .catch((err) => {
+    console.log("db error", err);
+  });
+
+const app = express();
+
+app.use(express.json());
+
+app.post("/auth/register", registerValidation, (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json(errors.array());
+  }
+  res.json({
+    success: true,
+  });
+});
+
+app.listen(4444, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log("Server OK");
+});
